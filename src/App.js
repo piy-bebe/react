ab2b20c4a2f67840187e48726d6abd0f210401b1
piy-bebe <piy.bebe@gmail.com>
2021-03-11 01:11:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Body />
+      <Technologies />
     </div>
   );
 }
-
-export default App;
+const Technologies = () => {
+  return (<div>
+    <ul>
+      <li>css</li>
+      <li>html</li>
+      <li>react</li>
+    </ul>
+  </div>)
+}
+const Header = () => {
+    return(<div>
+      <a href="#">Home</a>
+      <a href="#">News Feed</a>
+      <a href="#">Messages</a>
+    </div>)
+}
+const Body = () => {
+  return (<div>
+    <h1>Hello React!</h1>
+  </div>)
+}
+export default App
