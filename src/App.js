a8112e68b60c33a6c554caec8a45e72aebb0c832
be be <piy.bebe@gmail.com>
2021-03-19 15:48:05 +0300

    modified

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import './App.css'
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header>
+      <header className="header">
         <img
           src="https://cnet4.cbsistatic.com/img/Epbghr38kQNMsXPznUPXjedVpF4=/570x0/2018/07/30/f2932efe-cfd6-44ef-9fb6-65cde5010aab/mozilla-master-brand-firefox-fox-head.jpg"
           alt="logo"
         />
       </header>
-      <nav>
+      <nav className="nav">
         <div>Profile</div>
         <div>Messages</div>
       </nav>
-      <div>Main content</div>
+      <div className="content">Main content</div>
     </div>
   )
 }
