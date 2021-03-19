6776037c930ca25d1d199b816901b2a406dde4fc
be be <piy.bebe@gmail.com>
2021-03-19 14:25:28 +0300

    added main page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import "./App.css"
-import Header from './Header.js'
-import Body from './Body.js'
-import Technologies from './Technologies.js'
+import './App.css'
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Body />
-      <Technologies />
+    <div className="app-wrapper">
+      <header>
+        <img
+          src="https://cnet4.cbsistatic.com/img/Epbghr38kQNMsXPznUPXjedVpF4=/570x0/2018/07/30/f2932efe-cfd6-44ef-9fb6-65cde5010aab/mozilla-master-brand-firefox-fox-head.jpg"
+          alt="logo"
+        />
+      </header>
+      <nav>
+        <div>Profile</div>
+        <div>Messages</div>
+      </nav>
+      <div>Main content</div>
     </div>
-  );
+  )
 }
 export default App
--- src/Body.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-const Body = () => {
-  return (<div>
-    <h1>Hello React !</h1>
-  </div>)
-}
-
-export default Body
