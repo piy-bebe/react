d39efb419d3a3d9f13ee3543d13bbbafe8cde4f8
piy-bebe <piy.bebe@gmail.com>
2021-03-23 22:29:14 +0300

    modified app.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,13 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header'
 import Navbar from './components/Navbar'
+import Profile from './components/Profile'
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
       <Navbar />
-
-      <div className="content">
-        <div class="img"></div>
-        <div>ava + description</div>
-        <div>
-          My post
-          <div>New post</div>
-          <div>
-            <div>post 1</div>
-            <div>post 2</div>
-          </div>
-        </div>
-      </div>
+      <Profile />
     </div>
   )
 }
