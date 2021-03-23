f4eb478902e09c8643c77748629c2541ed3396df
piy-bebe <piy.bebe@gmail.com>
2021-03-23 22:29:25 +0300

    created Profile component

---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className="content">
+    <div class="img"></div>
+    <div>ava + description</div>
+    <div>
+      My post
+      <div>New post</div>
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Profile;
