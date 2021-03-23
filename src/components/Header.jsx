9989a3c2e1feca5d92d1cd4e02c8e87efac7e61e
piy-bebe <piy.bebe@gmail.com>
2021-03-23 22:18:58 +0300

    Created Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header">
+    <img
+      src="https://cnet4.cbsistatic.com/img/Epbghr38kQNMsXPznUPXjedVpF4=/570x0/2018/07/30/f2932efe-cfd6-44ef-9fb6-65cde5010aab/mozilla-master-brand-firefox-fox-head.jpg"
+      alt="logo"
+    />
+  </header>
+  )
+}
+
+export default Header;
