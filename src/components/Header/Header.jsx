45bc8eea2342e7ce68a984220476e650069c7c58
piy-bebe <piy.bebe@gmail.com>
2021-04-04 13:37:59 +0300

    css module Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import c from './Header.module.css'
 const Header = () => {
   return (
-    <header className="header">
+    <header className={c.header}>
     <img
       src="https://cnet4.cbsistatic.com/img/Epbghr38kQNMsXPznUPXjedVpF4=/570x0/2018/07/30/f2932efe-cfd6-44ef-9fb6-65cde5010aab/mozilla-master-brand-firefox-fox-head.jpg"
       alt="logo"
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (     
+  <nav className="nav">
+    <div className="item">
+      <a href="/#">Profile</a>
+    </div>
+    <div className="item">
+      <a href="/#">Message</a>
+    </div>
+    <div className="item">
+      <a href="/#">News</a>
+    </div>
+    <div className="item">
+      <a href="/#">Music</a>
+    </div>
+    <div className="item">
+      <a href="/#">Settings</a>
+    </div>
+  </nav>
+  )
+}
+
+export default Navbar;
